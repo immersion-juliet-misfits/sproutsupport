d2a438910de7526d1bc40c2416bff0192e1e2f64
jrob112 <josh.roberts0527@gmail.com>
2024-07-27 11:33:27 -0500

    update removed material ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
