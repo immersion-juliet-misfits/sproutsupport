678e492fcc34e6292a736bc2f0fb73f0df2ec49b
TreMartin-OS <tremartin3003@gmail.com>
2024-07-27 23:04:31 -0500

    Fix: Webpack errors from config no longer logging.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import path from 'path';
+import path, { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 import autoprefixer from 'autoprefixer';
-import 'dotenv/config';
-// import dotenv from 'dotenv';
-// import { load } from 'ts-dotenv';
-// const dotenv = require('dotenv');
-// require('dotenv').config();
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import sass from 'sass';
 
-module.exports = (env) => {
-  // Use env.<YOUR VARIABLE> here:
-  console.log('NODE_ENV: ', env.NODE_ENV); // e.g., 'local'
-  const isDev = env.NODE_ENV.includes('dev');
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default (env) => {
+  // console.log('NODE_ENV: ', process.env.NODE_ENV); // logs: correctly
+  const isDev = process.env.NODE_ENV.includes('dev');
 
   return {
     mode: isDev ? 'development' : 'production',
@@ -21,7 +23,7 @@ module.exports = (env) => {
     stats: {
       excludeModules: /node_modules/,
     },
-    entry: path.resolve(__dirname, './client/src/index.ts'),
+    entry: path.resolve(__dirname, './client/src/index.tsx'),
     output: {
       path: path.resolve(__dirname, './client/dist/'),
       publicPath: '/',
@@ -52,15 +54,12 @@ module.exports = (env) => {
           test: /\.(scss|css)$/,
           use: [
             {
-              // Adds CSS to the DOM by injecting a `<style>` tag
               loader: 'style-loader',
             },
             {
-              // Interprets `@import` and `url()` like `import/require()` and will resolve them
               loader: 'css-loader',
             },
             {
-              // Loader for webpack to process CSS with PostCSS
               loader: 'postcss-loader',
               options: {
                 postcssOptions: {
@@ -69,12 +68,9 @@ module.exports = (env) => {
               },
             },
             {
-              // Loads a SASS/SCSS file and compiles it to CSS
               loader: 'sass-loader',
-              // Added to suppress webpack sass deprecation warnings
-              // Remove if we need to fix them later
               options: {
-                implementation: require('sass'),
+                implementation: sass,
                 sassOptions: {
                   quietDeps: true,
                 },
@@ -92,106 +88,3 @@ module.exports = (env) => {
     ],
   };
 };
-
-/*
-
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-// import { load } from 'ts-dotenv';
-// const dotenv = require('dotenv');
-// require('dotenv').config();
-// require('dotenv').config();
-import dotenv from 'dotenv';
-
-
-
-// eslint-disable-next-line no-undef
-const { NODE_ENV } = process.env;
-const isDev = NODE_ENV.includes('dev');
-
-const config = {
-  mode: isDev ? 'development' : 'production',
-  devtool: isDev ? 'inline-source-map' : 'source-map',
-  watch: isDev,
-  stats: {
-    excludeModules: /node_modules/,
-  },
-  entry: path.resolve(__dirname, './client/src/index.ts'),
-  output: {
-    path: path.resolve(__dirname, './client/dist/'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|tsx)$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env'],
-            },
-          },
-          {
-            loader: 'ts-loader',
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader',
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader',
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
-            },
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader',
-            // Added to suppress webpack sass deprecation warnings
-            // Remove if we need to fix them later
-            options: {
-              // eslint-disable-next-line no-undef
-              implementation: require('sass'),
-              sassOptions: {
-                quietDeps: true,
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './client/index.html'),
-    }),
-    new NodePolyfillPlugin(),
-  ],
-};
-
-module.exports = config;
-
-
-*/
