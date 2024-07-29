38ce354f8a7119e16a8d343a45ae2534bc634392
TreMartin-OS <tremartin3003@gmail.com>
2024-07-29 11:05:31 -0500

    Update: Hello World is now displayed

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import App from './component/App';
+// import react from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
--- server/index.ts
+++ server/index.ts
@@ -6,10 +6,9 @@ import 'dotenv/config';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const app = express();
 const port = 8000;
-const DIST_PATH = path.resolve(__dirname, '../dist');
+const DIST_PATH = path.resolve(__dirname, '../client/dist');
 // Middleware to parse JSON bodies
 // Replaces body-parser
 app.use(express.json());
@@ -18,7 +17,5 @@ app.use(express.urlencoded({extended: false}));
 
 // Server to Serve Client
 app.use(express.static(DIST_PATH));
-
-
 
 app.listen(port, () => {console.log(`Listening on http://localhost:${port}`)})
