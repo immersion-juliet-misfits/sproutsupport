372804819b2cb32814b042745d8ea0a08961c494
Gage <gagecantrelle1899@gmail.com>
2024-07-30 13:55:22 -0500

    started seting up meetup

--- client/src/component/App.tsx
+++ client/src/component/App.tsx
@@ -1,3 +1,5 @@
-const App = () => <div>"Hello World: Welcome to Sprout Support"</div>;
+import Meetup from "./meetup/Meetup";
+
+const App = () => <div>"Hello World: Welcome to Sprout Support" <Meetup /></div>;
 
 export default App;
