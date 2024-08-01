2d001a2447169cff9223a2577c33a4c492fdb15e
TreMartin-OS <tremartin3003@gmail.com>
2024-08-01 11:45:44 -0500

    Fix: Prisma migration.  Add: migration script.

---
+++ prisma/migrations/20240801164431_fix_prisma_migrate_add_migrate_script/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `Plant` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[google_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Plant` DROP FOREIGN KEY `Plant_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `Plant` DROP COLUMN `userId`,
+    MODIFY `plantAPIID` INTEGER NULL,
+    MODIFY `species` VARCHAR(191) NULL,
+    MODIFY `commonName` VARCHAR(191) NULL,
+    MODIFY `nickname` VARCHAR(191) NULL,
+    MODIFY `location` VARCHAR(191) NULL,
+    MODIFY `imageUrl` VARCHAR(191) NULL,
+    MODIFY `description` VARCHAR(191) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_google_id_key` ON `User`(`google_id`);
