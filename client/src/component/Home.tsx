d73be9485b4544f66a4a1d6d14f9728a2446bdef
jrob112 <josh.roberts0527@gmail.com>
2024-08-02 15:16:06 -0500

    add Nav bar

--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -4,16 +4,20 @@ import { Box } from '@chakra-ui/react';
 import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import Post from './Post';
+import Nav from './NavBar';
 
 const Home = () => {
-  
+
   return (
+    <div>
+      <Nav />
     <Box>
       <ChakraLink as={ReactRouterLink} to='/createPost'>
         Create Post
       </ChakraLink>
       <Post />
     </Box>
+    </div>
   );
 };
 
---
+++ client/src/component/NavBar.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@chakra-ui/react';
+import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+
+const Nav = () => {
+  return (
+    <Box>
+      <ChakraLink as={ReactRouterLink} to='/home'>
+        Home
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to='/userprofile'>
+        Profile
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to='/meetup'>
+        Meet Up
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to='/createPost'>
+        Create Post
+      </ChakraLink>
+      </Box>
+  )
+}
+
+export default Nav;
