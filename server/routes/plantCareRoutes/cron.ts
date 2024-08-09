3bd02511c10b458fd2183a8a137bc919d3b61e29
ccaldwell11 <camroncldwll@gmail.com>
2024-08-08 19:17:51 -0500

    Update to cron job to use previous DB query again to get specific data for notifs.

--- server/routes/plantCareRoutes/cron.ts
+++ server/routes/plantCareRoutes/cron.ts
@@ -9,13 +9,18 @@ const job = new CronJob('*/9 * * * * *', () => { // wanna make this dynamic
   // console.log('rn', new Date());
 
   // overdue tasks || find them and update active to false(needs to be clicked to be true again/not overdue)
-  prisma.task.updateMany({where: { nextComplection: { lt: new Date() }, overdue: false }, data: { overdue: true }}) // marks tasks as overdue
-  // prisma.task.findMany({where: { nextComplection: { lt: new Date() }, overdue: false}})
+  // returns object with count
+  // prisma.task.updateMany({where: { nextComplection: { lt: new Date() }, overdue: false }, data: { overdue: true }}) // marks tasks as overdue
+  prisma.task.findMany({where: { nextComplection: { lt: new Date() }, overdue: false}}) // gives me all entries back
    .then((data) => {
-
+    
     // notify plant owner of their overdue task
-    if (data.count > 0) {
-      // console.log(data, 'OVERDUE') // count can be useful for displaying number of overdue tasks
+    if (data.length > 0) {
+      console.log(data, 'OVERDUE') // count can be useful for displaying number of overdue tasks
+      prisma.task.updateMany({where: { nextComplection: { lt: new Date() }, overdue: false }, data: {overdue: true}})
+        .then(() => {
+          console.log('updated')
+        })
     }
    })
 })
