2f133b6f2d5036d80e795254d12160c61fc9e617
TreMartin-OS <tremartin3003@gmail.com>
2024-08-16 19:52:39 -0500

    Git Merge commit

--- client/src/component/App.tsx
+++ client/src/component/App.tsx
@@ -22,9 +22,10 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   const toast = useToast()
-  const BUCKET_NAME = 'my1test1bucket';
-  // const BUCKET_NAME = 'sprout-support';
+  // const BUCKET_NAME = 'my1test1bucket';
+  const BUCKET_NAME = 'sprout-support';
   // const BUCKET_NAME = 'sproutsupportbucket'
+  // const BUCKET_NAME = 'ssupportbucket'
 
   const fetchUserData = () => {
     axios
